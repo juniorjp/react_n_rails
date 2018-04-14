2adf5bd8d4f062e0f8423a5283466ad35b054eec
Reginaldo Junior <juniorjp1989@gmail.com>
2018-04-14 14:36:09 -0300

    Apply thunk middleware, add auth reducer to store

--- app/javascript/packs/dogs_react.jsx
+++ app/javascript/packs/dogs_react.jsx
@@ -1,17 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
+import thunk from 'redux-thunk';
 import { createStore, applyMiddleware, combineReducers } from 'redux';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 
 import Signup from '../components/signup';
 import { reducer as formReducer, } from 'redux-form';
+import authReducer from '../reducers/authentication_reducer';
 
 const reducers = combineReducers({
   form: formReducer,
+  auth: authReducer
 });
 
-const store = createStore(reducers);
+const store = createStore(reducers, applyMiddleware(thunk));
 
 document.addEventListener('DOMContentLoaded', () => {
   ReactDOM.render(
