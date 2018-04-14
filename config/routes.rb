08133ac06a7c70a1226b63faadc5b15ef685b262
Reginaldo Junior <juniorjp1989@gmail.com>
2018-04-14 20:04:44 -0300

    redirect wildcard routes do home#index

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   root 'home#index'
+  get '*path', to: 'home#index'
   api_version(module: 'V1', header: { name: 'Accept', value: 'application/vnd.api+json; version=1' }, default: true) do
     post 'sign_up' => 'users#create'
 
