4e3df37dfb24c8f4eb20c47570ad89f3863ecc21
Reginaldo Junior <juniorjp1989@gmail.com>
2018-04-15 03:03:59 -0300

    Create user factory

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,5 +1,10 @@
 FactoryBot.define do
+  sequence :user_email do |n|
+    Faker::Internet.user_name + "#{n}@" + Faker::Internet.domain_name
+  end
   factory :user do
-    
+    username { Faker::Internet.user_name }
+    email { generate(:user_email) }
+    password Faker::Config.random.seed
   end
 end
