d3c7584d11ac205137b31604559fffb291c80e42
Reginaldo Junior <juniorjp1989@gmail.com>
2018-04-15 02:34:49 -0300

    fix spec

--- spec/controllers/v1/users_controller_spec.rb
+++ spec/controllers/v1/users_controller_spec.rb
@@ -3,7 +3,7 @@ require 'rails_helper'
 RSpec.describe V1::UsersController, :type => :controller do
   include ActiveJob::TestHelper
   describe 'POST signup' do
-
+    render_views
     it 'creates a user and returns a JWT token' do
       payload_info = { user: {
           username: 'jirico',
@@ -13,7 +13,7 @@ RSpec.describe V1::UsersController, :type => :controller do
       }
       @request.headers['Accept'] = 'application/vnd.api+json; version=1'
 
-      post :create, params: payload_info
+      post :create, params: payload_info, format: :json
       user = User.last
       parsed_response = JSON.parse(response.body).with_indifferent_access
       decoded_info = JWT.decode(parsed_response[:token], Rails.application.secrets.secret_key_base, 'HS256')[0].with_indifferent_access
