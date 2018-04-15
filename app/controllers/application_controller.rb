c4a561c27b186d9774f92e9bc3ab205f257a16eb
Reginaldo Junior <juniorjp1989@gmail.com>
2018-04-15 02:36:52 -0300

    Add authenticate_user before action and current_user helper method

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,15 @@
 class ApplicationController < ActionController::Base
+
+  def current_user
+    @current_user ||= params[:uid].nil? ? nil : User.find(params[:uid])
+  end
+
+  def authenticate_user
+    data = HashWithIndifferentAccess.new JWT.decode(request.env['HTTP_AUTHORIZATION'].split(' ').last, Rails.application.secrets.secret_key_base).first
+    params[:uid] = data[:sub]
+  rescue JWT::ExpiredSignature
+    head 403
+  rescue => e
+    head 400
+  end
 end
