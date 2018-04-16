7b806e250f3b6ab559dd9e611b5c27795a70e007
Reginaldo Junior <juniorjp1989@gmail.com>
2018-04-15 22:06:19 -0300

    Configure capistrano

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,39 +1,34 @@
-# config valid for current version and patch releases of Capistrano
-lock "~> 3.10.1"
+set :rvm_type, :user
+set :rvm1_ruby_version, "ruby-2.3.1"
+set :rvm1_map_bins, %w{rake gem bundle ruby}
 
-set :application, "my_app_name"
-set :repo_url, "git@example.com:me/my_repo.git"
+set :application, 'dogs'
+set :repo_url, 'git@github.com:juniorjp/react_n_rails.git'
 
-# Default branch is :master
-# ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
+set :branch, 'master'
+set :migration_role, :app
+set :use_sudo, false
+set :bundle_binstubs, nil
+set :unicorn_pid, '/home/jr/app/shared/pids/unicorn.dog.pid'
+set :linked_files, fetch(:linked_files, []).push('config/database.yml', 'config/application.yml')
+set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system')
 
-# Default deploy_to directory is /var/www/my_app_name
-# set :deploy_to, "/var/www/my_app_name"
+after 'deploy:publishing', 'deploy:restart'
 
-# Default value for :format is :airbrussh.
-# set :format, :airbrussh
+namespace :deploy do
+  task :restart do
+    invoke 'unicorn:restart'
+  end
 
-# You can configure the Airbrussh format using :format_options.
-# These are the defaults.
-# set :format_options, command_output: true, log_file: "log/capistrano.log", color: :auto, truncate: :auto
+  task :seed do
+    puts "\n=== Seeding Database ===\n"
+    on primary :db do
+      within current_path do
+        with rails_env: fetch(:stage) do
+          execute :rake, 'db:seed'
+        end
+      end
+    end
+  end
 
-# Default value for :pty is false
-# set :pty, true
-
-# Default value for :linked_files is []
-# append :linked_files, "config/database.yml", "config/secrets.yml"
-
-# Default value for linked_dirs is []
-# append :linked_dirs, "log", "tmp/pids", "tmp/cache", "tmp/sockets", "public/system"
-
-# Default value for default_env is {}
-# set :default_env, { path: "/opt/ruby/bin:$PATH" }
-
-# Default value for local_user is ENV['USER']
-# set :local_user, -> { `git config user.name`.chomp }
-
-# Default value for keep_releases is 5
-# set :keep_releases, 5
-
-# Uncomment the following to require manually verifying the host key before first deploy.
-# set :ssh_options, verify_host_key: :secure
+end
